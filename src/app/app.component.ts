bb4bdae22cd269fdf4d4dc3dbdc50619830301f7
Masvi <meetsjmasvi@hotmail.com>
2023-01-06 12:53:23 +0800

    angular template driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-tdf';
+  topics = ['Angular', 'React', 'Vue'];
+  userModel = new User('Rob', 'rob@test.com', '55533344', '', 'morning', true);
 }
